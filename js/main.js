153be70f659953bd79dfb46fb2df7f6d5ef31629
NandaCS <csnandaa@gmail.com>
2021-12-07 01:42:23 -0600

    star design, learn more, contact, join pages, transitions, text content

--- js/main.js
+++ js/main.js
@@ -23,12 +23,76 @@
     dl.appendChild(o)
   })
 
+  const lm = document.querySelector('#learnmorelink')
+  lm.addEventListener('click', () => {
+    const minfo = document.querySelector('#maininfo')
+    const lmore = document.querySelector('#learnmore')
+    minfo.style.display = 'none'
+    lmore.style.display = 'block'
+  })
+
+  const lm2 = document.querySelector('#learnmorelink')
+  lm2.addEventListener('click', () => {
+    const ctinfo = document.querySelector('#contactinfo')
+    const lmore = document.querySelector('#learnmore')
+    ctinfo.style.display = 'none'
+    lmore.style.display = 'block'
+  })
+
+  const lm3 = document.querySelector('#learnmorelink')
+  lm3.addEventListener('click', () => {
+    const join = document.querySelector('#join')
+    const lmore = document.querySelector('#learnmore')
+    join.style.display = 'none'
+    lmore.style.display = 'block'
+  })
+
   const ct = document.querySelector('#contactlink')
   ct.addEventListener('click', () => {
     const minfo = document.querySelector('#maininfo')
     const ctinfo = document.querySelector('#contactinfo')
     minfo.style.display = 'none'
     ctinfo.style.display = 'block'
+  })
+
+  const ct2 = document.querySelector('#contactlink')
+  ct2.addEventListener('click', () => {
+    const lmore = document.querySelector('#learnmore')
+    const ctinfo = document.querySelector('#contactinfo')
+    lmore.style.display = 'none'
+    ctinfo.style.display = 'block'
+  })
+
+  const ct3 = document.querySelector('#contactlink')
+  ct3.addEventListener('click', () => {
+    const join = document.querySelector('#join')
+    const ctinfo = document.querySelector('#contactinfo')
+    join.style.display = 'none'
+    ctinfo.style.display = 'block'
+  })
+
+  const jo = document.querySelector('#joinlink')
+  jo.addEventListener('click', () => {
+    const minfo = document.querySelector('#maininfo')
+    const join = document.querySelector('#join')
+    minfo.style.display = 'none'
+    join.style.display = 'block'
+  })
+
+  const jo2 = document.querySelector('#joinlink')
+  jo2.addEventListener('click', () => {
+    const lmore = document.querySelector('#learnmore')
+    const join = document.querySelector('#join')
+    lmore.style.display = 'none'
+    join.style.display = 'block'
+  })
+
+  const jo3 = document.querySelector('#joinlink')
+  jo3.addEventListener('click', () => {
+    const ctinfo = document.querySelector('#contactinfo')
+    const join = document.querySelector('#join')
+    ctinfo.style.display = 'none'
+    join.style.display = 'block'
   })
 
 // ..
